f55aa517c134d230a0889b207e9e9c798a326c20
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2023-10-04 11:49:43 +0600

    'update'

--- src/Components/UserForm.jsx
+++ src/Components/UserForm.jsx
@@ -109,7 +109,7 @@ export function UserFrom() {
             type="submit"
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           >
-            Button
+            Submit
           </button>
         </form>
       </div>
